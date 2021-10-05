937999dcf9b9474e38778429ea14fc195d410fe9
MisaelSantana <misaelsantana2011hotmail.com>
2021-10-05 15:37:21 -0300

    sem nome

--- challenge/app/views.py
+++ challenge/app/views.py
@@ -13,7 +13,7 @@ def home(request):
         request,
         'app/index.html',
         {
-            'title':'Home Page',
+            'title':'Challenge',
             'year':datetime.now().year,
         }
     )
