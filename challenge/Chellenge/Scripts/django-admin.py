d687e676409cd5cfba99abc4bde075d0b722b23c
MisaelSantana <misaelsantana2011hotmail.com>
2021-10-05 10:56:03 -0300

    modify project challenge

---
+++ challenge/Chellenge/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\misael.santana\Desktop\proj\challenge\challenge\Chellenge\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
