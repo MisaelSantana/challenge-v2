468e7b6ee46b08499f5c31ed8fe1564f35fe9214
MisaelSantana <misaelsantana2011hotmail.com>
2021-10-06 14:46:47 -0300

    commit att

---
+++ challenge/app/static/app/scripts/index.js
@@ -0,0 +1,16 @@
+// Functions JavaScript;
+
+// Function for create data;
+
+
+// Function for clear form;
+
+
+function clearForm() {
+    document.getElementById('partida').value=""
+    document.getElementById('destino').value=""
+    document.getElementById('km40').value=""
+    document.getElementById('km60').value=""
+    document.getElementById('km80').value=""
+    document.getElementById('km100').value=""
+}
